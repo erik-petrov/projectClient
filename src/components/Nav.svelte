04ab9ebea97fc9a95729d6c49626b0498bb58d20
kotem <kotem@DESKTOP-MSM5TRP>
2022-10-27 22:57:27 +0300

    As a user I see an overview page when I log on [Delivers #183581016]

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -64,6 +64,7 @@
 <nav>
 	<ul>
 		{#if $session.token}
+			<li><a aria-current="{segment === 'overview' ? 'page' : undefined}" href="overview">Overview</a></li>
 			<li><a aria-current="{segment === 'logout' ? 'page' : undefined}" href="{logout}" on:click|preventDefault={logout} >Log out</a></li>
 		{:else}
 			<li><a aria-current="{segment === 'login' ? 'page' : undefined}" href="login">Log in</a></li>
